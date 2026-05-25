f65038660feefcede86c7446a0f204fed3d9d099
”Danila <fiks11pikvik@gmai.com>
2026-05-25 23:19:25 +0600

    edit auth-service path

--- theme-project/auth-service/app/bot.py
+++ theme-project/auth-service/app/bot.py
@@ -7,7 +7,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN      = os.getenv("TELEGRAM_BOT_TOKEN")
-AUTH_SERVICE = "http://localhost:8000"
+AUTH_SERVICE = "http://auth-service:8000"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
